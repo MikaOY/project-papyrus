dd3251b15947acf98bf17ce6cceb2d92787c416d
Mika Yeap <mikaomarr@gmail.com>
2017-03-05 17:25:04 +0800

    Added creepy image

--- start.py
+++ start.py
@@ -4,8 +4,8 @@ from PIL import Image
 import webbrowser
 
 # Open image
-img = Image.open('/Users/Cotton/Desktop/project-papyrus/pineapple.png')
-img.show()
+#img = Image.open('/Users/Cotton/Desktop/project-papyrus/pineapple.png')
+#img.show()
 
 # Play song
-webbrowser.open("file:///Users/Mika/Desktop/Project%20Papyrus/song.mp3")
+webbrowser.open("song.mp3")
